24a70937f2ed368f0a55d2412dc663a5cd9f37e9
Jesse Cambon <jesse.cambon@gmail.com>
2021-09-11 19:33:25 -0400

    styler visualization cookbook

--- rmd_config.R
+++ rmd_config.R
@@ -2,11 +2,10 @@
 # run this file via source() in all RMarkdown files
 library(knitr)
 library(stringr)
-library(here)
 # get name of file during knitting and strip file extension
-rmd_filename <- str_remove(knitr::current_input(),"\\.Rmd")
+rmd_filename <- str_remove(knitr::current_input(), "\\.Rmd")
 
 # Figure path on disk = base.dir + fig.path
 # Figure URL online = base.url + fig.path
-knitr::opts_knit$set(base.dir = str_c(here::here(),'/'),base.url='../') # project root folder
-knitr::opts_chunk$set(fig.path = str_c("rmd_images/",rmd_filename,'/'),echo=TRUE)
+knitr::opts_knit$set(base.dir = str_c(here::here(), "/"), base.url = "../") # project root folder
+knitr::opts_chunk$set(fig.path = str_c("rmd_images/", rmd_filename, "/"), echo = TRUE)
